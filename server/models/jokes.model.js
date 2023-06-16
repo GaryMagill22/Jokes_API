5664d7ba3ed633e6cfe7452efac794963478f6b4
Gary Magill <Garymagill22@gmail.com>
2023-06-16 12:38:36 -0600

    Added punchline and setup validations

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose')
 const JokeSchema = new mongoose.Schema({
     setup: {
         type: String,
+        required: [true, 'Setup is required!'],
+        minlength: [10, "Setup must be atleast 10 characters long!"]
     },
     punchline: {
         type: String,
+        required: [true, "Punchline is required!"],
+        minlength: [3, "Punchline must be atleast 3 characters long!"]
     }
 }, { timestamps: true })
 
